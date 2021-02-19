ee231655fac07fadfa7f1d827fe8a3a64bbd40e8
Neil Davis <neil.a.davis@gmail.com>
2021-02-19 17:26:52 +0000

    Convert to hardware_PWM in pigpio client

--- pigpio/drv8835_driver_pigpio.py
+++ pigpio/drv8835_driver_pigpio.py
@@ -3,10 +3,9 @@ try:
 except RuntimeError:
     print("Error importing pigpio module! Please ensure pigpio and python[3]-pigpio are installed")
 
-# To keep source compatibility with Pololu's library using wiringPi we keep the range as -480 to 480
 # Motor speeds for this library are specified as numbers
 # between -MAX_SPEED and MAX_SPEED, inclusive.
-_max_speed = 480
+_max_speed = 1000000
 MAX_SPEED = _max_speed
 
 # Default GPIO pin assignments
@@ -14,8 +13,8 @@ MOTOR1_PWM_PIN = 12
 MOTOR1_DIR_PIN = 5
 MOTOR2_PWM_PIN = 13
 MOTOR2_DIR_PIN = 6
-# Default PWM frequency - Since pigpio uses hardware timing we can go quite high
-PWM_FREQUENCY=20000 # Up to 20 KHz, but actually depends on pigpio dameon start. See pigpiod -s
+# Default PWM frequency - Since pigpio uses hardware PWM we can go very high
+PWM_FREQUENCY=20000000 # 20 MHz, 
 # Default host and port - Local on default port 8888
 DEFAULT_HOST="127.0.0.1"
 DEFAULT_PORT=8888
@@ -32,17 +31,11 @@ def io_init(host=DEFAULT_HOST, port=DEFAULT_PORT):
     # Connect to the pigpio daemon
     the_pi=pigpio.pi(host, port)
     # Setup GPIO modes & PWM params
-    the_pi.set_mode(MOTOR1_PWM_PIN, pigpio.OUTPUT)
     the_pi.set_mode(MOTOR1_DIR_PIN, pigpio.OUTPUT)
-    the_pi.set_mode(MOTOR2_PWM_PIN, pigpio.OUTPUT)
     the_pi.set_mode(MOTOR2_DIR_PIN, pigpio.OUTPUT)
-    the_pi.set_PWM_frequency(MOTOR1_PWM_PIN, PWM_FREQUENCY)
-    the_pi.set_PWM_range(MOTOR1_PWM_PIN, MAX_SPEED)
-    the_pi.set_PWM_frequency(MOTOR2_PWM_PIN, PWM_FREQUENCY)
-    the_pi.set_PWM_range(MOTOR2_PWM_PIN, MAX_SPEED)
-    # Set initial speed to 0 (stopped)
-    the_pi.set_PWM_dutycycle(MOTOR1_PWM_PIN, 0)
-    the_pi.set_PWM_dutycycle(MOTOR1_PWM_PIN, 0)
+     # Set initial PWM speeds to 0 (stopped)
+    the_pi.hardware_PWM(MOTOR1_PWM_PIN, 0, 0)
+    the_pi.hardware_PWM(MOTOR2_PWM_PIN, 0, 0)
 
 def cleanup():
     """Global cleanup"""
@@ -65,7 +58,7 @@ class Motor(object):
             dir_value = 1
         if speed > MAX_SPEED:
             speed = MAX_SPEED
-        self.pi.set_PWM_dutycycle(self.pwm_pin, speed)
+        self.pi.hardware_PWM(self.pwm_pin, PWM_FREQUENCY, speed)
         self.pi.write(self.dir_pin, dir_value)
 
     def setSpeedPercent(self, speed):
